8d6c0d41497a2b735b2350a55e5cbacb84cdccc6
Quangha Ha <lecongthinh2005@gmail.com>
2026-05-14 13:59:36 +0700

    Buoi1

---
+++ CMS.Data/Entities/Customer.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Sinh vien: Le Cong Thinh
+ * MSSV: 2123110063
+ * Ngay tao:14-05-2026
+ * Version: 1.0
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    // Khách hàng
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Address { get; set; }
+
+        [Required]
+        public string Password { get; set; } // Lưu mật khẩu thô theo yêu cầu tối giản
+
+        public virtual ICollection<Order>? Orders { get; set; }
+    }
+}
+
---
+++ CMS.Data/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Sinh vien: Le Cong Thinh
+ * MSSV: 2123110063
+ * Ngay tao:14-05-2026
+ * Version: 1.0
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } // Quản trị viên hoặc Biên tập viên
+    }
+}
+
