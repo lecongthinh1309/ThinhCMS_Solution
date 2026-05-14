85c97a88c66d0b27e96409e8a29584f1325061af
Quangha Ha <lecongthinh2005@gmail.com>
2026-05-14 13:03:16 +0700

    Add project files.

---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Sinh vien: Le Cong Thinh
+ * MSSV: 2123110063
+ * Ngay tao:14-05-2026
+ * Version: 1.0
+ * 
+ */
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    //thuc the danh muc bai viet
+    public class Category
+    {
+        public int Id { get; set; } //ma danh muc bai viet, khoa chinh
+        public string Name { get; set; }  //ten danh muc bai viet
+        public string Description { get; set; } //mo ta danh muc bai viet
+        //Quan he: Mot danh muc co nhieu bai viet
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
