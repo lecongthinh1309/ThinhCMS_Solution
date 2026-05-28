ea3cc65ac5371147ef870b93c71a346a164f80fe
Quangha Ha <lecongthinh2005@gmail.com>
2026-05-28 13:16:57 +0700

    3

---
+++ CMS.Backend/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using CMS.Data; // Thay bằng Namespace của project Data
+
+public class AccountController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public AccountController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
